7494bd293fb638876a248f84c174fcfa38aeb707
mscoccimarro <11901885+mscoccimarro@users.noreply.github.com>
2018-09-09 13:07:10 -0300

    Quedaria modificar como se reparten los numero en base al gcl del 1.

--- tp/ej5.py
+++ tp/ej5.py
@@ -4,7 +4,19 @@ def generateRandomNumbers(a, c, m, times, u = 0):
   for i in range(times):
     u = ((a*u + c) % m) / m
     print('Iteration: ', i+1, ' Generated Number: ',  u)
+    convertToFixedNumber(u)
+
+
+def convertToFixedNumber(u):
+  if (u <= .5):
+    print('Generated Number: ', 1)
+  elif (u > .5 and u <= .7):
+    print('Generated Number: ', 2)
+  elif (u > .7 and u <= .8):
+    print('Generated Number: ', 3)
+  elif (u > .8 and u <= 1):
+    print('Generated Number: ', 4)
 
 promPadron = floor((94335 + 93784) / 2)
 
-generateRandomNumbers(1013904223, 1664525, 2**32, 10, promPadron)
+generateRandomNumbers(1013904223, 1664525, 2**32, 100, promPadron)
