479a4c7a1e39cee766a5bd167bb9e2fdd4d66a19
Lucas Risaro <lucasrisaro@gmail.com>
2018-09-11 21:07:16 -0300

    correccion de sitaxis

--- tp/ej4.py
+++ tp/ej4.py
@@ -12,7 +12,7 @@ probabilidadDeAceptar = list()
 #NORMAL DE MEDIA 35 Y VARIANZA 25
 def normal(x):
 
-    return ((exp(-0.5*(((x-35)/5)**2)))/sqrt(25*2*pi))
+    return ((exp(-0.5*(((x-35)/5)**2)))/sqrt(50*pi))
 
 
 def prob(x):
@@ -29,12 +29,14 @@ numsGauss = list()
 for i in range(0,n):
 
     r = np.random.uniform()
-    if r < p(n):
+    if r < probabilidadDeAceptar[n]:
         r2 = np.random.uniform()
         if r2 < 0.5:
             numsGauss.insert(i,numsExp[i])
         else:
             numsGauss.insert((i,(numsExp[i]*(-1))))
+
+print('Porcentaje de rechazo = ' + (n-len(numsGauss))/n)
 
 plt.hist(numsGauss)
 plt.show()
