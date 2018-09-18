8a2d9f55d7a68421e5989b1064fd711707e9af74
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-09-17 23:43:51 -0300

    Agrego metodo de Box-Muller para generar valores de la Normal Estandar.

--- tp/ej3.py
+++ tp/ej3.py
@@ -8,23 +8,28 @@ seed = int((94335 + 93784 + 96626)/3)
 # Método Lineal Congruente [Lehmer, 1949]
 #U(n) = (a*U(n-1) + c) mod m
 
-Un = {}
-Un[0] = seed
-
-def U(n):
-    if (n in Un) :
-        return Un[n]
-    else:
-        Un[n] = (a*U(n-1) + c) % m
-        return Un[n]
-
 def fNormal(x):
     return (math.sqrt((2*math.pi)))**-1 * math.exp(x**2/-2)
 
-def interpolarNormal():
-    return 1
+def vNormal(u1, u2):
+    return math.sqrt(-2*math.log(u1))*math.cos(u2)
 
-print(fNormal(1/2))
+def generateRandomUniform(a, c, m, seed):
+    u = (a*seed + c) % m
+    return {
+        "nextSeed": u,
+        "number": u/m
+    }
+
+for i in range(0, 10):
+    u1 = generateRandomUniform(a, c, m, seed)
+    seed = u1["nextSeed"]
+    u2 = generateRandomUniform(a, c, m, seed)
+    seed = u2["nextSeed"]
+    z1 = vNormal(u1["number"], u2["number"])
+    print('Generated u1 = ', u1["number"])
+    print('Generated u2 = ', u2["number"])
+    print('Generated z1 = ', z1)
 
 # 100.000 valores pseudo random generados por metodo de la transformada inversa
 # randomNums1 = list()
