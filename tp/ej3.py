97ccb4eea8f026f9e136188a1d408959ba4feb60
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-09-17 21:23:49 -0300

    Agrego funcion de densidad de la normal estandar y copio generador del ej1.

--- tp/ej3.py
+++ tp/ej3.py
@@ -1,15 +1,33 @@
-from math import log
-import matplotlib.pyplot as plt
+import math
 
-m = 2**32
-c = 1664525
+m = 2**32 
 a = 1013904223
-seed = 94059
-nums = list()
+c = 1664525
+seed = int((94335 + 93784 + 96626)/3)
 
-for i in range(0,100000):
+# Método Lineal Congruente [Lehmer, 1949]
+#U(n) = (a*U(n-1) + c) mod m
 
-    x = ((a*seed + c) % m)/m
-    nums.insert(i,x)
-    seed = x
+Un = {}
+Un[0] = seed
 
+def U(n):
+    if (n in Un) :
+        return Un[n]
+    else:
+        Un[n] = (a*U(n-1) + c) % m
+        return Un[n]
+
+def fNormal(x):
+    return (math.sqrt((2*math.pi)))**-1 * math.exp(x**2/-2)
+
+def interpolarNormal():
+    return 1
+
+print(fNormal(1/2))
+
+# 100.000 valores pseudo random generados por metodo de la transformada inversa
+# randomNums1 = list()
+# for i in range(0,100000):
+    # x = interpolarNormal()
+    # randomNums1.insert(i,x)
