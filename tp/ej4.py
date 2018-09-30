850cc614ff3c9f563baf0952eff50c19f54e1b3e
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-09-30 10:58:41 -0300

    Actualizo ej4 con cambios de la notebook.

--- tp/ej4.py
+++ tp/ej4.py
@@ -1,49 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats as st
-from math import sqrt, exp, pi
+from math import sqrt, exp, pi, log
 
-c= sqrt(2*exp(1)/pi)
+c = sqrt(2*exp(1)/pi)
 
-#GENERO N MUESTRAS EXPONENCIALES DE MEDIA 1
 n = 100000
-numsExp = list()
-probabilidadDeAceptar = list()
-numsGauss = list() #ACA GUARDO LOS VALORES ACEPTADOS COMO GAUSSIANOS
+aceptados = list() #VALORES ACEPTADOS SEGUN DISTRIBUCIÓN NORMAL, MEDIA 35 Y DESVÍO 5
 
 #NORMAL DE MEDIA 35 Y VARIANZA 25
-def normal(x):
+def X(t):
+    return exp(-0.5*(((t-35)/5)**2))/(5*sqrt(2*pi))
 
-    return ((exp(-0.5*(((x-35)/5)**2)))/sqrt(50*pi))
+def Y(t):
+    return exp(-t)
 
-#PROBABILIDAD DE ACPTAR EL VALOR GENERADO
-def prob(x):
-
-    return (normal(x)/(c*exp(-x)))
-
-#GENERO MUESTRAS DE EXPONENCIALES DE MEDIA 1 Y GENERO UN VECTOR DE PROBABILIDADES
-#DE ACEPTAR CADA VALOR GENERADO
-for i in range(0,n):
-    t = np.random.exponential(1)
-    numsExp.insert(i, t)
-    probabilidadDeAceptar.insert(i,prob(t))
+def prob(t):
+    return (X(t)/(c*Y(t)))
 
 for i in range(0,n):
-    r = np.random.uniform()
+    u = np.random.uniform()
+    
+    j = (log(1-(np.random.uniform())))*(-1)
 
-    if r > probabilidadDeAceptar[i]:
-        r2 = np.random.uniform()
-        if r2 < 0.5:
-            numsGauss.insert(i,numsExp[i])
+    if(u > prob(j)):
+        if(u >= 0.5):
+            aceptados.append((j*5)+35)
         else:
-            numsGauss.insert(i,numsExp[i]*(-1))
+            aceptados.append(((-1*j)*5)+35)
 
-print('Porcentaje de rechazo = ' + str((n-len(numsGauss))/n))
-
-
-print('Varianza = ' + str(np.var(numsGauss)))
-print('Media = ' + str(np.median(numsGauss)))
-print('Moda = ' + str(st.mode(numsGauss)))
-
-plt.hist(numsGauss,bins=1000)
-plt.show()
+print('Varianza = ' + str(np.var(aceptados)))
+print('Media = ' + str(np.median(aceptados)))
+print('Moda = ' + str(st.mode(aceptados)[0][0]))
+print('cantidad de aceptados ' + str(len(aceptados)))
