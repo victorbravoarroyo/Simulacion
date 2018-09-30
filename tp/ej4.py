1a0ebb0604f56bdf61a8e4aaf51f41bfe865fc39
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-09-30 11:35:20 -0300

    Corrijo método para que genere ~Normal(0,1). Transformo para que sea ~Normal(35,5).

--- tp/ej4.py
+++ tp/ej4.py
@@ -10,7 +10,8 @@ aceptados = list() #VALORES ACEPTADOS SEGUN DISTRIBUCIÓN NORMAL, MEDIA 35 Y DES
 
 #NORMAL DE MEDIA 35 Y VARIANZA 25
 def X(t):
-    return exp(-0.5*(((t-35)/5)**2))/(5*sqrt(2*pi))
+    return exp(-0.5*t)/(sqrt(2*pi))
+    # return exp(-0.5*(((t-35)/5)**2))/(5*sqrt(2*pi))
 
 def Y(t):
     return exp(-t)
@@ -18,18 +19,25 @@ def Y(t):
 def prob(t):
     return (X(t)/(c*Y(t)))
 
-for i in range(0,n):
-    u = np.random.uniform()
-    
-    j = (log(1-(np.random.uniform())))*(-1)
+def transformL(x, a, b):
+    return (x*a) + b
 
-    if(u > prob(j)):
+count = 1
+maxAceptado = 100000
+while count <= maxAceptado:
+    u = np.random.uniform()
+    j = np.random.exponential(1)
+
+    if(u < prob(j)):
+        # print('aceptado: ', count)
+        count += 1
         if(u >= 0.5):
-            aceptados.append((j*5)+35)
+            aceptados.append( transformL(j, 5/3, 35) )
+            # aceptados.append(j)
         else:
-            aceptados.append(((-1*j)*5)+35)
+            aceptados.append( transformL(j*-1, 5/3, 35) )
+            # aceptados.append((-1*j))
 
 print('Varianza = ' + str(np.var(aceptados)))
 print('Media = ' + str(np.median(aceptados)))
 print('Moda = ' + str(st.mode(aceptados)[0][0]))
-print('cantidad de aceptados ' + str(len(aceptados)))
