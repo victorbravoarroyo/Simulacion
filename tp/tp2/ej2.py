c3ff7a740a2104d4290da858c10e8c11d065c8ce
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-10-27 11:03:11 -0300

    Verificar que la matriz de transicion de estados este bien.

---
+++ tp/tp2/ej2.py
@@ -0,0 +1,9 @@
+## 3 estados
+# 1 - Esperando
+# 2 - Arribo Solicitud
+# 3 - Termina Proceso Solicitud
+##
+#     [ p11 p12 p13 ]   [ 113/120 1/40 1/30 ]
+# P = [ p21 p22 p23 ] = [ 1 0 0 ]
+#     [ p31 p32 p33 ]   [ 1 0 0 ]
+
