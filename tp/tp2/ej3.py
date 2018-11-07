f53523ece22bfc8bb38a3af077bd33241fc531fd
makushi <maximiliano.scoccimarro@gmail.com>
2018-11-06 22:59:52 -0300

    Punto 1 del ej3 resuelto.

---
+++ tp/tp2/ej3.py
@@ -0,0 +1,22 @@
+# 1) 2*xt = xt-1 + 2*yt-1
+# 2) yt = yt-1 - xt-1/2
+# 3) zt = zt-1 - xt-1 - yt-1
+######################################################
+# Entonces busco los puntos de equilibrio:
+######################################################
+# 1) xeq = xt = xt-1
+# 2) yeq = yt = yt-1
+# 3) zeq = zt = zt-1
+######################################################
+# Reemplazando:
+######################################################
+# 1) 2*xeq = xeq + 2*yeq
+# 2) yeq = yeq - xeq/2
+# 3) zeq = zeq - xeq - yeq
+#
+# 1) xeq = 2*yeq
+# 2) xeq = 0, yeq = 0
+######################################################
+# Entonces los puntos de equilibrio del sistema son:
+######################################################
+# (xeq, yeq, zeq) = (0, 0, zeq), para todo zeq que pertenece a los reales
